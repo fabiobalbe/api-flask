68d096c2a1665b910e5894db99e356a8ef524998
Fábio Balbé <fabiobalbe@MacBook-Air-de-Fabio.local>
2023-09-15 11:23:45 -0300

    Configuração inicial

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+def fetch_spacex_launches():
+    url = "https://api.spacexdata.com/v4/launches"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+
+launches = fetch_spacex_launches()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
